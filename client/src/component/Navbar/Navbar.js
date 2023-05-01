1aff9e748aeac3ef404ae8eb17a894f08f08c3bd
sup25 <suparnaa51@gmail.com>
2023-05-01 22:26:47 +0545

    updated nav

--- client/src/component/Navbar/Navbar.js
+++ client/src/component/Navbar/Navbar.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Logo from '../../Assets/logo'
+import Login from '../Login'
+import { Register } from '../Register'
 
 const Navbar = () => {
     return (
         <>
-            <div className='bg-[#0d5b46] h-20 flex' >
-                <Logo className="w-60" />
+            <div className='bg-[#0d5b46] h-20 section flex justify-between   items-center' >
+                <Logo className="w-44 h-auto" />
+                <div className='flex text-white gap-4  '>
+                    Home
+                    About
+                    Services
+                    Contact
+                    <Login />
+                    <Register />
+                </div>
             </div>
         </>
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,12 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         "./app/**/*.{js,ts,jsx,tsx}",
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
-        "./utils/**/*.{js,ts,jsx,tsx}",
-        "./constants/**/*.{js,ts,jsx,tsx}",
+
 
         // Or if using `src` directory:
         "./src/**/*.{js,ts,jsx,tsx}",
@@ -15,10 +16,35 @@ module.exports = {
             fontFamily: {
                 'sans': ['DM Sans', 'sans-serif'],
             },
+
+            maxWidth: {
+                default: "1440px",
+            },
+            color: {
+                "0d5b46": "#0d5b46",
+            },
         },
     },
-    plugins: [],
+    plugins: [
+        plugin(function ({ addComponents, theme }) {
+            addComponents({
+                ".section": {
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    padding: "0 10px",
+                    "@screen sm": {
+                        padding: "0 30px",
+                    },
+                },
+                ".container": {
+                    maxWidth: theme("maxWidth.default"),
+                },
+            })
+
+        })
+    ],
 }
 
-//1366x768
+//18:9 - 720x1440, 1080x2160, 1440x2880
 //0d5b46
