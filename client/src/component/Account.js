ed85a8989998672d0091b04cf03854717becad89
sup25 <suparnaa51@gmail.com>
2023-05-21 22:32:26 +0545

    initialized profile route

--- client/src/component/Account.js
+++ client/src/component/Account.js
@@ -1,28 +1,56 @@
+import React, { useState } from "react";
 import { useFetchUser } from "../Api/Api";
 import { AiFillCamera } from "react-icons/ai";
 
 const Account = () => {
   const user = useFetchUser();
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleProfilePictureUpload = async () => {
+    if (selectedFile) {
+      try {
+        const formData = new FormData();
+        formData.append("profilePic", selectedFile);
+
+        await fetch("http://localhost:5000/profilePic", {
+          method: "POST",
+          body: formData,
+        });
+      } catch (error) {
+        console.error("Error uploading profile picture:", error);
+      }
+    }
+  };
 
   return (
-    <div className="section py-20   ">
+    <div className="section py-20">
       <div className="flex py-5 flex-col px-5 justify-evenly md:flex-row w-full h-auto gap-10 bg-white rounded-2xl shadow-xl">
         <div className="flex flex-col justify-center items-center">
           <div className="w-[300px] h-[250px] flex flex-col items-center justify-center bg-gray-300 rounded-t-xl shadow-xl">
-            <div className=" text-3xl">{user}</div>
+            <div className="text-3xl">{user}</div>
           </div>
-          <div className="w-full h-12 gap-2 justify-center text-xl text-white font-bold  rounded flex  items-center bg-[#0d5b46]">
+          <label
+            htmlFor="fileInput"
+            className="w-[300px] cursor-pointer h-12 gap-2 justify-center text-xl text-white font-bold rounded flex items-center bg-[#0d5b46]"
+          >
             <AiFillCamera />
-            <span className="flex justify-center items-center">
+            <span
+              onClick={handleProfilePictureUpload}
+              className="flex justify-center items-center"
+            >
               Change your picture
             </span>
-          </div>
+          </label>
+          <input
+            id="fileInput"
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            onChange={(e) => setSelectedFile(e.target.files[0])}
+          />
         </div>
 
-        <form
-          className=" md:w-1/2 w-full height-fit px-3 flex items-center"
-          // onSubmit={handleSubmit}
-        >
+        <form className="md:w-1/2 w-full height-fit px-3 flex items-center">
           <div className="flex flex-col gap-5 w-full h-auto">
             <div className="text-2xl md:text-4xl">
               Change your account credential
@@ -33,8 +61,6 @@ const Account = () => {
               name="username"
               required
               placeholder="Name"
-              // value={formData.username}
-              // onChange={handleChange}
               className="w-full h-auto py-2 rounded-2xl flex px-4 border border-[#0d5b46]"
             />
             <input
@@ -42,8 +68,6 @@ const Account = () => {
               name="email"
               required
               placeholder="Email"
-              // value={formData.email}
-              // onChange={handleChange}
               className="w-full h-auto py-2 rounded-2xl flex px-4 border border-[#0d5b46]"
             />
             <input
@@ -51,8 +75,6 @@ const Account = () => {
               name="password"
               required
               placeholder="Password"
-              // value={formData.password}
-              // onChange={handleChange}
               className="w-full h-auto py-2 rounded-2xl flex px-4 border border-[#0d5b46]"
             />
             <input
@@ -60,8 +82,6 @@ const Account = () => {
               name="reenterpassword"
               required
               placeholder="Reenter your Password"
-              // value={formData.password}
-              // onChange={handleChange}
               className="w-full h-auto py-2 rounded-2xl flex px-4 border border-[#0d5b46]"
             />
             {/* {errorMessage && <div className="text-red-500">{errorMessage}</div>} */}
