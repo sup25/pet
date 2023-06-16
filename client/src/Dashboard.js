2694727443d441dbd725891c740559a8759fbbef
sup25 <suparnaa51@gmail.com>
2023-06-16 23:26:51 +0545

    updated dashboard

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -4,6 +4,7 @@ import { BsFillBookmarkHeartFill, BsFillPatchPlusFill } from "react-icons/bs";
 
 const Dashboard = () => {
   const [posts, setPosts] = useState([]);
+
   const [formData, setFormData] = useState({
     petName: "",
     petType: "",
@@ -39,14 +40,92 @@ const Dashboard = () => {
       <div className="container mx-auto pb-20 px-4">
         <div className="flex justify-end gap-2">
           <div
-            className="bg-gray-200 p-4 rounded flex shadow justify-center items-center cursor-pointer"
+            className="bg-gray-20 bg-[#0d5b46] hover:bg-[#107359]  p-4 rounded flex shadow justify-center items-center cursor-pointer"
             onClick={toggleForm}
           >
-            <BsFillPatchPlusFill className="w-8 h-auto" />
+            <BsFillPatchPlusFill className="w-8 h-auto text-white " />
           </div>
-          <div className="bg-gray-200 p-4 rounded flex shadow justify-center items-center">
-            <BsFillBookmarkHeartFill className="w-8 h-auto" />
+          <div className=" bg-[#0d5b46] hover:bg-[#107359] p-4 rounded flex shadow justify-center items-center">
+            <BsFillBookmarkHeartFill className="w-8 h-auto text-white " />
           </div>
+        </div>
+        <div
+          className={`p-4 mt-4 tooltip="Add new post" gap-4 rounded flex justify-center items-center ${
+            clickFill ? "shadow" : ""
+          }`}
+        >
+          {clickFill && (
+            <div className="md:flex-nowrap flex-wrap flex justify-between w-full">
+              <div className="flex flex-col w-full justify-center items-center gap-2">
+                <h2 className="text-2xl font-semibold">Upload Photo</h2>
+                <div className="w-[300px] h-[250px] rounded bg-gray-200"></div>
+                <button
+                  type="submit"
+                  className="bg-[#0d5b46] hover:bg-[#107359] text-white py-2 px-4 rounded font-medium"
+                >
+                  Upload
+                </button>
+              </div>
+              <div className="mt-8 w-full flex flex-col flex-wrap ">
+                <h2 className="text-2xl font-semibold mb-4">Create a Post</h2>
+                <form onSubmit={handleFormSubmit} className="space-y-4">
+                  <div>
+                    <label
+                      htmlFor="petName"
+                      className="block mb-2 font-semibold"
+                    >
+                      Pet Name:
+                    </label>
+                    <input
+                      type="text"
+                      id="petName"
+                      name="petName"
+                      value={formData.petName}
+                      onChange={handleInputChange}
+                      className="w-full px-4 py-2 border rounded"
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="petType"
+                      className="block mb-2 font-semibold"
+                    >
+                      Pet Type:
+                    </label>
+                    <input
+                      type="text"
+                      id="petType"
+                      name="petType"
+                      value={formData.petType}
+                      onChange={handleInputChange}
+                      className="w-full px-4 py-2 border rounded"
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="petDescription"
+                      className="block mb-2 font-semibold"
+                    >
+                      Pet Description:
+                    </label>
+                    <textarea
+                      id="petDescription"
+                      name="petDescription"
+                      value={formData.petDescription}
+                      onChange={handleInputChange}
+                      className="w-full px-4 py-2 border rounded"
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="bg-[#0d5b46] hover:bg-[#107359] text-white font-medium py-2 px-4 rounded"
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
+            </div>
+          )}
         </div>
         <div className="mt-8">
           <h2 className="text-2xl font-semibold mb-4">Recent Activity</h2>
@@ -70,60 +149,6 @@ const Dashboard = () => {
               </li>
             ))}
           </ul>
-          {clickFill && (
-            <div className="mt-8">
-              <h2 className="text-2xl font-semibold mb-4">Create a Post</h2>
-              <form onSubmit={handleFormSubmit} className="space-y-4">
-                <div>
-                  <label htmlFor="petName" className="block mb-2 font-semibold">
-                    Pet Name:
-                  </label>
-                  <input
-                    type="text"
-                    id="petName"
-                    name="petName"
-                    value={formData.petName}
-                    onChange={handleInputChange}
-                    className="w-full px-4 py-2 border rounded"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="petType" className="block mb-2 font-semibold">
-                    Pet Type:
-                  </label>
-                  <input
-                    type="text"
-                    id="petType"
-                    name="petType"
-                    value={formData.petType}
-                    onChange={handleInputChange}
-                    className="w-full px-4 py-2 border rounded"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="petDescription"
-                    className="block mb-2 font-semibold"
-                  >
-                    Pet Description:
-                  </label>
-                  <textarea
-                    id="petDescription"
-                    name="petDescription"
-                    value={formData.petDescription}
-                    onChange={handleInputChange}
-                    className="w-full px-4 py-2 border rounded"
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="bg-[#0d5b46] hover:bg-[#107359] text-white py-2 px-4 rounded"
-                >
-                  Submit
-                </button>
-              </form>
-            </div>
-          )}
         </div>
       </div>
     </div>
