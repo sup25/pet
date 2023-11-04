ae7d0728b63e43d5475b576d3e8bfe2aeb404128
sup25 <suparnaa51@gmail.com>
2023-11-04 20:19:22 +0545

    attempt on profile picture

--- client/src/Context/ProfileContext.js
+++ client/src/Context/ProfileContext.js
@@ -1,13 +1,28 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const ProfileContext = createContext();
 
 export const ProfileProvider = ({ children }) => {
-  const [profilePicture, setProfilePicture] = useState(null);
+  const [profilePicture, setProfilePicture] = useState(() => {
+    // Try to retrieve the profile picture from local storage on component initialization
+    const storedProfilePicture = localStorage.getItem("profilePicture");
+    return storedProfilePicture ? storedProfilePicture : null;
+  });
 
   const updateProfilePicture = (imageURL) => {
     setProfilePicture(imageURL);
+
+    // Store the updated profile picture in local storage
+    localStorage.setItem("profilePicture", imageURL);
   };
+
+  // Optionally, you can use a cleanup effect to remove the profile picture from local storage when the user logs out
+  useEffect(() => {
+    return () => {
+      // Clear the profile picture from local storage when the user logs out
+      localStorage.removeItem("profilePicture");
+    };
+  }, []);
 
   return (
     <ProfileContext.Provider value={{ profilePicture, updateProfilePicture }}>
